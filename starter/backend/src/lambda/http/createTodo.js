339593a632e7a1404af683455ce49f675ea590f2
zdtz <dduythien.it@gmail.com>
2024-05-05 11:20:39 +0700

    update review

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -14,14 +14,6 @@ export const handler = middy()
   .handler(async (event) => {
     const userId = getUserId(event);
     const newTodo = JSON.parse(event.body)
-    if (!newTodo.name) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          'message': "Name must be at least 1 character"
-        })
-      }
-    }
     const todo = await createTodoAction(userId, newTodo);
 
     return JSON.stringify({
